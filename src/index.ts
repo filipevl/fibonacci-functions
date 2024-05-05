093eaf72faf238b2cd3b98a3aee577737c6cded7
Filipe Lacerda <filipe@vcasistemas.com.br>
2024-05-05 18:45:36 -0300

    chore: export functions

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { generateFibonacciSequence } from "./functions/generateFibonacciSequence";
+import { getFibonacciValueAtPosition } from "./functions/getFibonacciValueAtPosition";
+
+export default {
+	generateFibonacciSequence,
+	getFibonacciValueAtPosition
+};
